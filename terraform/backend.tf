41c136e3e01929cf2fa3cbbebf0b93a558fe8fc3
simlf <lefournsimon@gmail.com>
2025-03-27 10:21:32 +0100

    feat: add ci terraform validate

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-group-015"
+    resource_group_name  = var.resource_group_name
     storage_account_name = "bucketterraform"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
